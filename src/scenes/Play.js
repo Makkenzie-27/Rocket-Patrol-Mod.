73c543cabbc1ede29c53eedbca9b16723b135781
Makkenzie Zepeda <Makkenzie1@ymail.com>
2021-04-13 17:44:24 -0600

    Creating and Adding Assets

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,8 +2,32 @@ class Play extends Phaser.Scene {
     constructor() {
       super("playScene");
     }
+
+    preload() {
+        // load images/tile sprites
+        this.load.image('rocket', './assets/Rocket.PNG');
+        this.load.image('spaceship', './assets/Spaceship.PNG');
+        this.load.image('starfield', './assets/GalaxyField.png');
+      }
     
+
+
+
     create() {
-      this.add.text(20, 20, "Rocket Patrol Menu");
+
+    // green UI background
+    this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+
+
+    // Neon Pink borders
+    this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFF00CC).setOrigin(0, 0);
+    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFF00CC).setOrigin(0, 0);
+    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xCC0099).setOrigin(0, 0);
+    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xCC0099).setOrigin(0, 0);
+      
+
+
+
+
     }
   }
