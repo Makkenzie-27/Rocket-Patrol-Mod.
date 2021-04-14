bb77df8b8f087bcf7bcad5bdf2520a32fa4d0169
Makkenzie Zepeda <Makkenzie1@ymail.com>
2021-04-14 17:26:44 -0600

    Changes the background color and border design.

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,6 +8,7 @@ class Play extends Phaser.Scene {
         this.load.image('rocket', './assets/Rocket.PNG');
         this.load.image('spaceship', './assets/Spaceship.PNG');
         this.load.image('starfield', './assets/GalaxyField.png');
+        this.load.image('neonborder', './assets/NeonBorder.PNG');
       }
     
 
@@ -19,14 +20,13 @@ class Play extends Phaser.Scene {
     this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
 
     // green UI background
-    this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+    this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x82EEFD).setOrigin(0, 0);
+
+    //place new border art
+    this.neonborder = this.add.tileSprite(0, 0, 640, 480, 'neonborder').setOrigin(0, 0);
 
 
-    // Neon Pink borders
-    this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFF00CC).setOrigin(0, 0);
-    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFF00CC).setOrigin(0, 0);
-    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xCC0099).setOrigin(0, 0);
-    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xCC0099).setOrigin(0, 0);
+   
       }
 
       update() {
